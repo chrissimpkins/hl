bfc1e2f9c7231fb3fa2e75a9fc7e9f287d0faed1
Chris Simpkins <git.simpkins@gmail.com>
2020-01-17 21:42:30 -0500

    add highlighters tests

--- src/highlighters/mod.rs
+++ src/highlighters/mod.rs
@@ -46,7 +46,43 @@ pub fn get_theme(user_request: &str) -> String {
 }
 
 pub fn highlight_line(line: &str, hl: &mut HighlightLines, ss: &SyntaxSet) -> String {
-    let ranges: Vec<(Style, &str)> = hl.highlight(&line, ss);
+    let ranges: Vec<(Style, &str)> = hl.highlight(line, ss);
 
     as_24_bit_terminal_escaped(&ranges[..], true)
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    const TESTLINE: &str = r#"      <coord axis="opsz" value="11.0"/>"#;
+    const EXPECTED_LINE: &str = "\u{1b}[48;2;45;45;45m\u{1b}[38;2;211;208;200m      \u{1b}[48;2;45;45;45m\u{1b}[38;2;211;208;200m<\u{1b}[48;2;45;45;45m\u{1b}[38;2;242;119;122mcoord\u{1b}[48;2;45;45;45m\u{1b}[38;2;211;208;200m \u{1b}[48;2;45;45;45m\u{1b}[38;2;249;145;87maxis\u{1b}[48;2;45;45;45m\u{1b}[38;2;211;208;200m=\u{1b}[48;2;45;45;45m\u{1b}[38;2;211;208;200m\"\u{1b}[48;2;45;45;45m\u{1b}[38;2;153;204;153mopsz\u{1b}[48;2;45;45;45m\u{1b}[38;2;211;208;200m\"\u{1b}[48;2;45;45;45m\u{1b}[38;2;211;208;200m \u{1b}[48;2;45;45;45m\u{1b}[38;2;249;145;87mvalue\u{1b}[48;2;45;45;45m\u{1b}[38;2;211;208;200m=\u{1b}[48;2;45;45;45m\u{1b}[38;2;211;208;200m\"\u{1b}[48;2;45;45;45m\u{1b}[38;2;153;204;153m11.0\u{1b}[48;2;45;45;45m\u{1b}[38;2;211;208;200m\"\u{1b}[48;2;45;45;45m\u{1b}[38;2;211;208;200m/>";
+
+    #[test]
+    fn test_get_theme_default() {
+        assert_eq!(crate::highlighters::get_theme("default"), crate::highlighters::THEME_DEFAULT);
+    }
+
+    #[test]
+    fn test_get_theme_dark() {
+        assert_eq!(crate::highlighters::get_theme("dark"), crate::highlighters::THEME_DARK);
+    }
+
+    #[test]
+    fn test_get_theme_light() {
+        assert_eq!(crate::highlighters::get_theme("light"), crate::highlighters::THEME_LIGHT);
+    }
+
+    #[test]
+    fn test_highlight_line_xml() {
+        let ss = syntect::parsing::SyntaxSet::load_defaults_newlines();
+        let ts = syntect::highlighting::ThemeSet::load_defaults();
+        let syntax = ss
+            .find_syntax_by_token("xml")
+            .unwrap_or_else(|| ss.find_syntax_by_token("txt").unwrap());
+        let mut hl = syntect::easy::HighlightLines::new(syntax, &ts.themes["base16-eighties.dark"]);
+        let escaped_string = crate::highlighters::highlight_line(TESTLINE, &mut hl, &ss);
+        assert_eq!(escaped_string, EXPECTED_LINE);
+    }
+
 }
